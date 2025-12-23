08dbefeb2b85596185df39e4959274bb496513c9
zhihang.hu <huzhihang32@gmail.com>
2025-12-23 16:21:01 +0800

    feat: 添加 useCallback 导入并更新 Vite 配置以监听所有接口

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Listens on all interfaces
+    // OR
+    // host: true, // Also listens on all interfaces
+  },
 })
